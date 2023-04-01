89e1987bc8b73f1ada15234175983ac01142cd1d
Douglas <douglas.wdias@hotmail.com>
2023-04-01 10:29:34 -0300

    fix: fixed bad request error

--- BuberBreakfast/ServiceErrors/Errors.Breakfast.cs
+++ BuberBreakfast/ServiceErrors/Errors.Breakfast.cs
@@ -11,13 +11,13 @@ public static class Errors
       description: "Breakfast not Found"
     );
 
-    public static Error InvalidName => Error.NotFound(
+    public static Error InvalidName => Error.Validation(
       code: "Breakfast.InvalidName",
       description: $"Breakfast name must be between {Models.Breakfast.MinNameLength} and {Models.Breakfast.MaxNameLength} characters"
     );
 
-    public static Error InvalidDescription => Error.NotFound(
-      code: "Breakfast.InvalidName",
+    public static Error InvalidDescription => Error.Validation(
+      code: "Breakfast.InvalidDescription",
       description: $"Breakfast description must be between {Models.Breakfast.MinDescriptionLength} and {Models.Breakfast.MaxDescriptionLength} characters"
     );
   }
